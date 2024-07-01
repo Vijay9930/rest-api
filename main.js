a30cfbc6a81eb4d7b1171c44b4a812c436ebafd2
My Name <vijay@sportalytics.in>
2024-07-01 16:13:18 +0530

    start api

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 import express from "express";
-
+import kpireports from "./routes/kpi-reports.js";
 
 const app = express();
 const PORT = 6969;
 
-
-
 app.get('/', (req, res) => {
     res.json({ msg: "Hello Students" });
 });
+
+
+app.use("/kpi-reports", kpireports)
 
 app.listen(PORT, () => {
     console.log(`The Server is running at http:localhost:${PORT}`);
